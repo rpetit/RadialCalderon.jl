edada677481ade6af817cde201814e116b7ac448
Romain Petit <petitromain@protonmail.com>
2025-05-22 22:10:17 +0200

    Update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,8 @@ using Documenter
 using DocumenterCitations
 using Literate
 
+bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"); style=:authoryear)
+
 open(joinpath(joinpath(@__DIR__, "src"), "index.md"), "w") do io
     println(
         io,
@@ -17,32 +19,32 @@ open(joinpath(joinpath(@__DIR__, "src"), "index.md"), "w") do io
     end
 end
 
-# examples_jl_path = joinpath(dirname(@__DIR__), "examples")
-# examples_md_path = joinpath(@__DIR__, "src", "examples")
+examples_jl_path = joinpath(dirname(@__DIR__), "examples")
+examples_md_path = joinpath(@__DIR__, "src", "examples")
 
-# for file in readdir(examples_md_path)
-#     if endswith(file, ".md")
-#         rm(joinpath(examples_md_path, file))
-#     end
-# end
+for file in readdir(examples_md_path)
+    if endswith(file, ".md")
+        rm(joinpath(examples_md_path, file))
+    end
+end
 
-# for file in readdir(examples_jl_path)
-#     Literate.markdown(joinpath(examples_jl_path, file), examples_md_path)
-# end
+for file in readdir(examples_jl_path)
+    Literate.markdown(joinpath(examples_jl_path, file), examples_md_path)
+end
 
-# function literate_title(path)
-#     l = first(readlines(path))
-#     return l[3:end]
-# end
+function literate_title(path)
+    l = first(readlines(path))
+    return l[3:end]
+end
 
 pages = [
     "Home" => "index.md",
-    # "Examples" => [
-    #     "Example 1" => joinpath("examples", "example_1.md"),
-    # ],
+    "Tutorials" => [
+        "Forward map" => joinpath("examples", "forward.md")
+    ],
     "API reference" => "api.md"
 ]
 
-makedocs(sitename="RadialCalderon.jl", pages=pages)
+makedocs(;sitename="RadialCalderon.jl", pages=pages, plugins=[bib])
 
 deploydocs(repo="github.com/rpetit/RadialCalderon.jl")
